e1c832389e322c225cfe0505c4313389e6005958
Anmol Maini <anmol.maini@gmail.com>
2024-07-18 15:12:03 +0530

    new site

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,115 @@
-import Image from "next/image";
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Button } from "@/components/ui/button";
 
-export default function Home() {
+type Company = {
+  name: string;
+  url: string;
+};
+
+// TODO: Move this to a CMS
+const PORTFOLIO: Company[] = [
+  { name: "SuperFam", url: "https://www.superfam.app/" },
+  { name: "VAMA", url: "https://vama.app/" },
+  { name: "Dice", url: "https://dice.tech/" },
+  { name: "Tramline", url: "https://www.tramline.app/" },
+  { name: "Roopya", url: "https://roopya.money/" },
+  { name: "Segwise", url: "https://www.segwise.ai/" },
+  { name: "Closing Theory", url: "https://closingtheory.com/" },
+  { name: "CarePay", url: "https://carepay.money/" },
+  { name: "Defog", url: "https://defog.ai/" },
+  { name: "Integration Labs", url: "https://www.integrationlabs.co/" },
+  { name: "Induced", url: "https://www.induced.ai/" },
+  { name: "DPDzero", url: "https://www.dpdzero.com/" },
+  { name: "Requestly", url: "https://requestly.io/" },
+  {
+    name: "Togai (Acq. $ZUO)",
+    url: "https://www.togai.com/zuora-acquires-togai/",
+  },
+  {
+    name: "Partykit (Acq. $NET)",
+    url: "https://blog.partykit.io/posts/partykit-is-joining-cloudflare",
+  },
+  { name: "Spiti (Exited)", url: "https://spiti.xyz/" },
+];
+
+const Home: NextPage = () => {
+  const subject = "We're building {your_company_name}";
+  const body = "Hey! We would love to tell you more about what we're building.";
+  const mailtoLink = `mailto:pitch@untitledventures.xyz?subject=${encodeURIComponent(
+    subject
+  )}&body=${encodeURIComponent(body)}`;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <main>
+        <div className="mx-auto max-w-6xl py-8 px-4 sm:py-24 sm:px-6 lg:px-8">
+          <div className="sm:align-center sm:flex sm:flex-col">
+            <h1 className="text-center text-4xl font-extrabold sm:text-6xl">
+              &#34;UNTITLED VENTURES&#34;
+            </h1>
+          </div>
+          <div className="p-2">
+            <div className="border-accents-1 m-auto my-8 w-full max-w-3xl rounded-md border border-black p-4">
+              <div className="flex flex-col gap-8 p-8">
+                <h3 className="text-2xl">
+                  <span className="font-bold">UNTITLED VENTURES</span> is an
+                  early-stage venture firm started by{" "}
+                  <a href="https://x.com/anmolm_" className="hover:text-white">
+                    Anmol
+                  </a>{" "}
+                  &{" "}
+                  <a
+                    href="https://x.com/vedicakant"
+                    className="hover:text-white"
+                  >
+                    Vedica
+                  </a>
+                  .
+                </h3>
+                <h3 className="text-2xl">
+                  We believe start-ups have only begun to scratch the surface of
+                  their potential contribution to the Indian economy -- a story
+                  that is still playing out and is, as yet,{" "}
+                  <span className="font-bold italic">untitled</span>.
+                </h3>
+                <h3 className="text-2xl">
+                  We are excited to play a small role in this story and invest
+                  $25k in early-stage Indian founders. If you are starting a
+                  company or thinking of it, please reach out
+                </h3>
+                <div className="flex justify-center">
+                  <a href={mailtoLink}>
+                    <Button variant="default" size="lg" className="text-lg">
+                      Pitch Us
+                    </Button>
+                  </a>
+                </div>
+                <h3 className="text-2xl">
+                  Our current portfolio includes:{" "}
+                  <ul className="flex flex-col list-inside">
+                    {PORTFOLIO.map((co, i) => (
+                      <li className="list-disc" key={co.name}>
+                        <a
+                          href={co.url}
+                          className="hover:text-white font-bold text-lg"
+                        >
+                          {co.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </h3>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </main>
+    </>
   );
-}
+};
+
+export default Home;
